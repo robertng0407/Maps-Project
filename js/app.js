b8c45e83908583baf4a51f63c10db69eeab8534a
Robert Nguyen <robertng0407@gmail.com>
2017-08-09 16:42:21 -0700

    Add list of coffee shops and functionality to show marker

--- js/app.js
+++ js/app.js
@@ -1,24 +1,78 @@
-var locations = data;
+var data = data;
 
 
 var ViewModel = function() {
-  this.showListings = function() {
-    var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-      bounds.extend(markers[i].position);
+  this.menu = new Menu();
+
+  this.locations = ko.observableArray();
+
+  data.forEach((location) => {
+    this.locations.push(new CoffeeShop(location))
+  })
+
+  this.locations().sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
     }
-    map.fitBounds(bounds);
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0
+  })
+
+  // This function will loop through the markers array and display them all.
+  this.showListings = function() {
+    showListings();
+  }
+
+  // This function will loop through the listings and hide them all.
+  this.hideListings = function() {
+    hideMarkers(markers)
+  };
+
+  this.toggleDrawing = function() {
+    toggleDrawing(drawingManager);
+  }
+
+  this.zoomToArea = function() {
+    zoomToArea();
+  }
+
+  this.searchWithinTime = function() {
+    searchWithinTime();
+  }
+
+  // Listen for the event fired when the user selects a prediction and clicks
+  // "go" more details for that place.
+  this.textSearchPlaces = function() {
+    textSearchPlaces();
+  }
+
+  this.toggleDisplay = function() {
+    console.log(this.locations.length)
+    this.menu.optionsBoxVisible(!this.menu.optionsBoxVisible());
+    this.menu.coffeeShopList(!this.menu.coffeeShopList());
+  }
+
+  this.viewLocation = function(data) {
+    // console.log(data);
+    markers.forEach((marker) => {
+      if (data.id === marker.id) {
+        showListing(marker);
+      }
+    })
   }
 };
 
+var Menu = function() {
+  this.optionsBoxVisible = ko.observable(false);
+  this.coffeeShopList = ko.observable(true);
+}
+
 var CoffeeShop = function(data) {
   this.title = data.title;
-  this.location = {
-    lat: data.lat,
-    lng: data.lng
-  }
+  this.location = data.location;
+  this.id = data.id;
 }
 
 ko.applyBindings(new ViewModel())
--- js/data.js
+++ js/data.js
@@ -4,139 +4,159 @@ var data = [
     location: {
       lat: 45.52371399999999,
       lng: -122.67478790000001
-    }
+    },
+    id: 0
   },
   {
     title: 'World Cup Coffee & Tea @ Powell\'s',
     location: {
       lat: 45.52307769999999,
       lng: -122.68193029999998
-    }
+    },
+    id: 1
   },
   {
     title: 'Stumptown Coffee Roasters',
     location: {
       lat: 45.5217794,
       lng: -122.67344170000001
-    }
+    },
+    id: 2
   },
   {
     title: '40 LBS Coffee Bar',
     location: {
       lat: 45.516943,
       lng: -122.674935
-    }
+    },
+    id: 3
   },
   {
     title: 'Peet\'s Coffee',
     location: {
       lat: 45.5205586,
       lng: -122.6788105
-    }
+    },
+    id: 4
   },
   {
     title: 'Case Study Coffee Roasters',
     location: {
       lat: 45.5190805,
       lng: -122.68234269999999
-    }
+    },
+    id: 5
   },
   {
     title: 'Oak St. Coffee',
     location: {
       lat: 45.5216678,
       lng: -122.6781805
-    }
+    },
+    id: 6
   },
   {
     title: 'Einstein Bros. Bagels',
     location: {
       lat: 45.5173745,
       lng: -122.67739059999997
-    }
+    },
+    id: 7
   },
   {
     title: 'Stumptown Coffee Roasters',
     location: {
       lat: 45.5220349,
       lng: -122.6817173
-    }
+    },
+    id: 8
   },
   {
     title: 'Peet\'s Coffee',
     location: {
       lat: 45.52356299999999,
       lng: -122.68256500000001
-    }
+    },
+    id: 9
   },
   {
     title: 'Barista',
     location: {
       lat: 45.5192121,
       lng: -122.67537959999999
-    }
+    },
+    id: 10
   },
   {
     title: 'Courier Coffee',
     location: {
       lat: 45.5228038,
       lng: -122.6804985
-    }
+    },
+    id: 11
   },
   {
     title: 'Floyd\'s Coffee Shop Old Town',
     location: {
       lat: 45.5237594,
       lng: -122.67177079999999
-    }
+    },
+    id: 12
   },
   {
     title: 'City Coffee',
     location: {
       lat: 45.5166387,
       lng: -122.67738759999997
-    }
+    },
+    id: 13
   },
   {
     title: 'Simple. Local. Coffee',
     location: {
       lat: 45.5219764,
       lng: -122.67176489999997
-    }
+    },
+    id: 14
   },
   {
     title: 'Water Avenue Coffee',
     location: {
       lat: 45.5182951,
       lng: -122.679395
-    }
+    },
+    id: 15
   },
   {
     title: 'Starbucks',
     location: {
       lat: 45.5170493,
       lng: -122.67993680000001
-    }
+    },
+    id: 16
   },
   {
     title: 'Seattle\'s Best Coffee',
     location: {
       lat: 45.51592979999999,
       lng: -122.67967520000002
-    }
+    },
+    id: 17
   },
   {
     title: 'Starbucks',
     location: {
       lat: 45.5191697,
       lng: -122.6795411
-    }
+    },
+    id: 18
   },
   {
     title: 'The Fresh Pot',
     location: {
       lat: 45.5207616,
       lng: -122.6796784
-    }
+    },
+    id: 19
   }
 ]
